107e09c654756fcdd8d39f827cc9813b59cdf32a
Marcin Szczepkowski <m.szczepkowski@hotmail.com>
2024-01-12 18:20:20 +0100

    Added top curve additional color settings

--- build/blocks/curvyblock/index.asset.php
+++ build/blocks/curvyblock/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '7a9fbffdf8a48818d77f');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '064f45285aa30026cd5b');
