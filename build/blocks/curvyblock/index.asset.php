091fa6fbb3981f8f11276106e2dcef09b77c018c
Marcin Szczepkowski <m.szczepkowski@hotmail.com>
2024-01-13 15:55:17 +0100

    Added previews to Curvy Block with heading and paragraph

--- build/blocks/curvyblock/index.asset.php
+++ build/blocks/curvyblock/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'bd4603c7b26c45fa5a6b');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '374465aa60e305a4a39d');
