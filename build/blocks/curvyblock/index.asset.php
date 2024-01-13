e175ac6eb6f73a9ada955e520401fef4405a6ef3
Marcin Szczepkowski <m.szczepkowski@hotmail.com>
2024-01-13 15:48:30 +0100

    Added variations to Curvy Block

--- build/blocks/curvyblock/index.asset.php
+++ build/blocks/curvyblock/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'c7c5222837a16c34e83b');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'bd4603c7b26c45fa5a6b');
