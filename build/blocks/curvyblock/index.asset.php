a96cd6bbe53a8a79a00c9cd109fc408e9ab0437b
Marcin Szczepkowski <m.szczepkowski@hotmail.com>
2024-01-13 13:51:47 +0100

    Added svg icon to Curvy Block

--- build/blocks/curvyblock/index.asset.php
+++ build/blocks/curvyblock/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'b773b984f95957d0a187');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '8cd2a1f9db9e65898567');
